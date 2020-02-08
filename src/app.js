e1c4ee6dab88f7a64fcbcc5166b95742e6576ba5
Dominick <dominickveil@gmail.com>
2020-02-08 18:10:21 +0100

    Static HTML and styling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-export const run = () => console.log('running')
+export const run = () => {
+  const shell = new ActiveXObject('WScript.Shell')
+     shell.run('http://www.google.com')
+  console.log('ran shell')
+ }
