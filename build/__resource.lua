1a05a7943b59b49aa18e17fb839aab199ab20e94
Dominick <dominickveil@gmail.com>
2020-02-10 21:08:03 +0100

    Build 1.0.0

---
+++ build/__resource.lua
@@ -0,0 +1,24 @@
+files {
+  'index.html',
+  'main.css',
+  'main.js',
+  'song.ogg',
+  'img/logo.png',
+  'img/cursor__main.png',
+  'img/r1.png',
+  'img/r2.png',
+  'img/r3.png',
+  'img/r4.png',
+  'img/r5.png',
+  'img/r6.png',
+  'img/rulesTitle.png',
+  'img/heli.jpg',
+  'img/lazer.jpg',
+  'img/asset.jpg',
+  'fonts/ChaletLondon.ttf',
+  'fonts/Pricedown.ttf',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
